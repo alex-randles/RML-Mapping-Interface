72bed1e1e4704d5368c2b88099f26018656c4ec7
Alex Randles <alexrandles0@gmail.com>
2023-10-28 19:21:24 +0100

    updated requirements file

--- waitress_serve.py
+++ waitress_serve.py
@@ -1,5 +1,6 @@
 # server to run python code on virtual machine
 import waitress
 import main
-waitress.serve(main.app, host='0.0.0.0', port=9000)
 print("Waitress server running.....")
+waitress.serve(main.app, host='0.0.0.0', port=9000)
+
